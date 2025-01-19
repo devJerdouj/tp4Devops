ac98ab62f5a748ed30fa86175185b57dc7eff4e2
hamza jerdouj <hamza.jerdouj@gmail.com>
2025-01-19 10:43:08 +0100

    tp4 v1

---
+++ src/main/java/org/example/tp4jenkins/Tp4JenkinsApplication.java
@@ -0,0 +1,13 @@
+package org.example.tp4jenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp4JenkinsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Tp4JenkinsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/tp4jenkins/Tp4JenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.tp4jenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp4JenkinsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
